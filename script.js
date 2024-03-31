9bd2353e824a270964fc3b6572e95b1f61ed6b64
Bruno Pouyenne <Bruno Pouyenne>
2024-03-31 05:52:46 +0200

    adaptation aux tailles variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 onload = () => {
-    go("jeux");
+    go("jeux","jeux");
     resetChrono();
 }
 
@@ -9,18 +9,31 @@ onpopstate = (event) => {
     go(ecran, section)
 }
 
+let currentSection;
 function go(ecran,section) {
+    if (section) currentSection = section; 
     
     for (divEcran of document.querySelectorAll(".ecran")) {
         if (divEcran.id == ecran) {
-            divEcran.style.display = "flex"
-            divEcran.className = "ecran"+ (section ? " "+section : "")
+            divEcran.style.display = "flex";
+            divEcran.className = "ecran "+ currentSection;
         } 
         else {
-            divEcran.style.display = "none"
+            divEcran.style.display = "none";
         }
     }
-    history.pushState([ecran, section], "")
+    history.pushState([ecran, currentSection], "")
+}
+let liensMiniJeux = [
+    "https://wordwall.net/fr/resource/31172804/tap-h%c3%a9-1",
+    "https://wordwall.net/fr/resource/65660529/tape-h%c3%a9-3",
+    "https://wordwall.net/fr/resource/65660297/tap%c3%a9-h%c3%a9-2",
+    "https://wordwall.net/fr/resource/65660878/tape-h%c3%a9-4",
+    "https://wordwall.net/fr/resource/65661228/tap-h%c3%a9-5",
+    "https://wordwall.net/fr/resource/65661625"
+]
+function minijeu(i){
+    location = liensMiniJeux[i-1];
 }
 
 /* Gestion du Chonomètre */
